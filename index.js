d01ebd95eed3850ee80d7a532f92a38a21804ef2
mostafiz <rafi11zxy@gmail.com>
2022-10-27 14:16:27 +0600

    catagory api send

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const catagories = require('./categories.json');
+
+
+app.use(cors());
 
 app.get('/', (req,res) => {
     res.send('server is running okay');
 });
+
+app.get('/categories', (req,res) => {
+    res.send(catagories);
+})
+
+
 
 app.listen(port, ()=> {
     console.log('education news server', port);
