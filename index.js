4ffbb75e6338f3e90219b6c73e9aa9982d5eeb8c
mostafiz <rafi11zxy@gmail.com>
2022-10-27 21:58:38 +0600

    vercel update

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const catagories = require('./categories.json');
+const courses = require('./courses.json');
+
 
 
 app.use(cors());
@@ -14,6 +16,25 @@ app.get('/', (req,res) => {
 
 app.get('/categories', (req,res) => {
     res.send(catagories);
+})
+
+app.get('/courses/:id', (req,res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectCourse = courses.find(n => n._id === id);
+    res.send(selectCourse)
+});
+
+app.get('/catagory/:id', (req,res)=>{
+    const id = req.params.id;
+    const selectCatagory = courses.find(n => n.category_id ===id);
+    res.send(selectCatagory);
+
+});
+
+
+app.get('/course', (req,res) => {
+    res.send(courses);
 })
 
 
